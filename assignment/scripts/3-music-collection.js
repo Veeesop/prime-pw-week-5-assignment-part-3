c8c9dbae6b80ffe2f3ae5c2a167604ce9ac0ced8
Alexander Ratanas <alexanderratanas@gmail.com>
2022-04-26 09:52:38 -0500

    added addToCollection function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,2 +1,16 @@
 console.log("***** Music Collection *****");
 let collection = [];
+
+const addToCollection = (title, artist, yearPublished) => {
+  const newEntry = {
+    title: title,
+    artist: artist,
+    yearPublished: yearPublished,
+  };
+  collection.push(newEntry);
+  return newEntry;
+};
+
+addToCollection("Black Sails in the Sunset", "AFI", "1999");
+
+console.log(collection);
