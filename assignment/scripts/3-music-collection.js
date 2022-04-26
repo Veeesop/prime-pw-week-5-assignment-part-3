06f61484734ea336374bca3105d2fa7e4174ed1e
Alexander Ratanas <alexanderratanas@gmail.com>
2022-04-26 10:16:41 -0500

    tested addToCollection function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -11,6 +11,13 @@ const addToCollection = (title, artist, yearPublished) => {
   return newEntry;
 };
 
-addToCollection("Black Sails in the Sunset", "AFI", "1999");
+console.log(addToCollection("Black Sails in the Sunset", "AFI", "1999"));
+console.log(addToCollection("Jettison Mind Hatch", "Tipper", "2019"));
+console.log(
+  addToCollection("My Tennessee Mountain Home", "Dolly Parton", "1973")
+);
+console.log(addToCollection("9 to 5 and Odd Jobs", "Dolly Parton", "1980"));
+console.log(addToCollection("Stay Trippy", "Juicy J", "2013"));
+console.log(addToCollection("Reign in Blood", "Slayer", "1986"));
 
 console.log(collection);
