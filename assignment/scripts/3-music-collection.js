c40514e969915020937d0985d9a74a44d91b8311
Alexander Ratanas <alexanderratanas@gmail.com>
2022-04-28 13:45:05 -0500

    finished first strech goal

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -11,14 +11,14 @@ const addToCollection = (title, artist, yearPublished) => {
   return newEntry;
 };
 
-console.log(addToCollection("Black Sails in the Sunset", "AFI", "1999"));
-console.log(addToCollection("Jettison Mind Hatch", "Tipper", "2019"));
+console.log(addToCollection("Black Sails in the Sunset", "AFI", 1999));
+console.log(addToCollection("Jettison Mind Hatch", "Tipper", 2019));
 console.log(
-  addToCollection("My Tennessee Mountain Home", "Dolly Parton", "1973")
+  addToCollection("My Tennessee Mountain Home", "Dolly Parton", 1973)
 );
-console.log(addToCollection("9 to 5 and Odd Jobs", "Dolly Parton", "1980"));
-console.log(addToCollection("Stay Trippy", "Juicy J", "2013"));
-console.log(addToCollection("Reign in Blood", "Slayer", "1986"));
+console.log(addToCollection("9 to 5 and Odd Jobs", "Dolly Parton", 1980));
+console.log(addToCollection("Stay Trippy", "Juicy J", 2013));
+console.log(addToCollection("Reign in Blood", "Slayer", 1986));
 
 console.log(collection);
 
@@ -45,3 +45,17 @@ const findByArtist = (artist) => {
 
 console.log(findByArtist("Dolly Parton"));
 console.log(findByArtist("Toby Keith"));
+
+function search(artist = "none", year = "none") {
+  let searchResult = [];
+  if (artist === "none" || year === "none") {
+    return (searchResult = collection);
+  }
+  searchResult = collection.filter(function (n) {
+    return n.artist === artist && n.yearPublished === year;
+  });
+  return searchResult;
+}
+
+console.log(search("Dolly Parton", 1980));
+console.log(search("Toby Keith", 2020));
