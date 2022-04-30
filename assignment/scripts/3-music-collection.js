58a24481b2ba94d1b2d6f5310379f45cf7a1cbab
Alexander Ratanas <alexanderratanas@gmail.com>
2022-04-30 13:51:39 -0500

    updated show collection

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,33 +1,75 @@
 console.log("***** Music Collection *****");
 let collection = [];
 
-const addToCollection = (title, artist, yearPublished) => {
+addToCollection = function (title, artist, yearPublished, tracks) {
   const newEntry = {
     title: title,
     artist: artist,
     yearPublished: yearPublished,
+    tracks: tracks,
   };
   collection.push(newEntry);
   return newEntry;
 };
 
-console.log(addToCollection("Black Sails in the Sunset", "AFI", 1999));
-console.log(addToCollection("Jettison Mind Hatch", "Tipper", 2019));
 console.log(
-  addToCollection("My Tennessee Mountain Home", "Dolly Parton", 1973)
+  addToCollection("Black Sails in the Sunset", "AFI", 1999, [
+    { trackName: "Strength Through Wounding", trackDuration: "1:33" },
+    { trackName: "Porphyria Cutanea Tarda", trackDuration: "2:07" },
+    { trackName: "Exsanguination", trackDuration: "2:48" },
+  ])
 );
-console.log(addToCollection("9 to 5 and Odd Jobs", "Dolly Parton", 1980));
-console.log(addToCollection("Stay Trippy", "Juicy J", 2013));
-console.log(addToCollection("Reign in Blood", "Slayer", 1986));
+console.log(
+  addToCollection("Jettison Mind Hatch", "Tipper", 2019, [
+    { trackName: "Sayonara", trackDuration: "4:27" },
+    { trackName: "Vection", trackDuration: "4:07" },
+    { trackName: "Exit Chapel Perilous", trackDuration: "3:08" },
+  ])
+);
+console.log(
+  addToCollection("My Tennessee Mountain Home", "Dolly Parton", 1973, [
+    { trackName: "The Letter", trackDuration: "2:03" },
+    { trackName: "I Remember", trackDuration: "342" },
+    { trackName: "Old Black Kettle", trackDuration: "2:32" },
+  ])
+);
+console.log(
+  addToCollection("9 to 5 and Odd Jobs", "Dolly Parton", 1980, [
+    { trackName: "9 to 5", trackDuration: "2:45" },
+    { trackName: "Hush-a-bye Hard Times", trackDuration: "3:48" },
+    { trackName: "The House of the Rising Sun", trackDuration: "4:02" },
+  ])
+);
+console.log(
+  addToCollection("Stay Trippy", "Juicy J", 2013, [
+    { trackName: "Stop It", trackDuration: "3:21" },
+    { trackName: "Smokin Rollin", trackDuration: "2:36" },
+    { trackName: "No Heart No Love", trackDuration: "4:03" },
+  ])
+);
+console.log(
+  addToCollection("Reign in Blood", "Slayer", 1986, [
+    { trackName: "Angel of Death", trackDuration: "4:52" },
+    { trackName: "Piece by Piece", trackDuration: "2:03" },
+    { trackName: "Necrophobic", trackDuration: "1:41" },
+  ])
+);
 
 console.log(collection);
 
-const showCollection = (arr) => {
+showCollection = function (arr) {
   console.log(arr.length);
-  for (item of arr) {
+  for (let i = 0; i < arr.length; i++) {
     console.log(
-      `${item.title} by ${item.artist}, published in ${item.yearPublished}.`
+      `${arr[i].title} by ${arr[i].artist}, published in ${arr[i].yearPublished}. `
     );
+    for (let j = 0; j < arr[i].tracks.length; j++) {
+      console.log(
+        `${j + 1}. ${arr[i].tracks[j].trackName}: ${
+          arr[i].tracks[j].trackDuration
+        }`
+      );
+    }
   }
 };
 
@@ -59,3 +101,4 @@ function search(artist = "none", year = "none") {
 
 console.log(search("Dolly Parton", 1980));
 console.log(search("Toby Keith", 2020));
+console.log(search());
